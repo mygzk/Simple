dd8419a019024a0f502e9da4841f2e57c38939c4
guozhk <1565619659@qq.com>
2017-12-10 10:34:46 +0800

    test

--- app/src/main/java/com/example/simple/test/BranchTest.java
+++ app/src/main/java/com/example/simple/test/BranchTest.java
@@ -1,8 +1,12 @@
 package com.example.simple.test;
 
+import com.example.simple.utils.StringCheckUtil;
+
 /**
  * Created by guozhk on 2017/12/10.
  */
 
 public class BranchTest {
+
+    private String s;
 }

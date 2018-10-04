162f80f4b5725c9f27102da3e775491e4c5dc7a2
guozhk <1565619659@qq.com>
2018-10-04 19:49:07 +0800

    modify item

--- app/src/main/java/com/example/simple/widget/BiddingItemLayout.java
+++ app/src/main/java/com/example/simple/widget/BiddingItemLayout.java
@@ -7,6 +7,7 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.text.TextUtils;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -23,43 +24,27 @@ import com.example.simple.R;
  * Date : 2018/10/4
  * Dre :发标文件item
  **/
-
-/**
- * <declare-styleable name="bindding_item">
- * <p>
- * <attr name="content_hint" format="string" />
- * <attr name="content_hint_color" format="string" />
- * <attr name="hint_ev_text_size" format="integer" />
- * <attr name="hint_ev_text_color" format="color" />
- * <p>
- * <attr name="img_right" format="reference" />
- * <p>
- * <attr name="type" format="enum">
- * <enum name="tv" value="0" />
- * <enum name="ev" value="1" />
- * </attr>
- * <p>
- * <attr name="secondColor" format="color" />
- * </declare-styleable>
- */
 public class BiddingItemLayout extends FrameLayout {
     private int TYPE_TEXT = 0;
     private int TYPE_EDIT = 1;
 
     //label内容
     private String mLabelText;
-    private int mLabelSize = 14;
+    private int mLabelSize = dip2px(getContext(), 14);
     private int mLabelColor;
 
     private String mContent;
-    private int mTvContentSize;
+    private int mTvContentSize = dip2px(getContext(), 14);
+
     private int mTvContentColor;
     private int mEtContentSize;
     private int mEtContentColor;
     private String mHintContent;
-    private int mHintSize;
+    private int mHintSize = dip2px(getContext(), 14);
+
     private int mHintColor;
     private int mImgRight = R.mipmap.ic_launcher;
+    private boolean mImageRightVisible;
     private int mType;
 
     private RelativeLayout rootView;
@@ -69,6 +54,9 @@ public class BiddingItemLayout extends FrameLayout {
     private ImageView imgRight;
     private View lineView;
 
+
+    private IContentClickListener mIContentClickListener;
+    private IRightImgClickListener mIRightImgClickListener;
 
     public BiddingItemLayout(@NonNull Context context) {
         this(context, null);
@@ -84,6 +72,9 @@ public class BiddingItemLayout extends FrameLayout {
                 defStyleAttr, 0);
         int n = ta.getIndexCount();
         mLabelColor = getResources().getColor(R.color.blue_font);
+        mTvContentColor = getResources().getColor(R.color.blue_font);
+        mEtContentColor = getResources().getColor(R.color.blue_font);
+        mHintColor = getResources().getColor(R.color.blue_font);
 
         for (int i = 0; i < n; i++) {
             int attr = ta.getIndex(i);
@@ -95,10 +86,10 @@ public class BiddingItemLayout extends FrameLayout {
                     mLabelColor = ta.getColor(attr, getResources().getColor(R.color.blue_font));
                     break;
                 case R.styleable.bindding_item_label_text_size:
-                /*    mLabelSize = (int) ta.getDimension(attr, TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 14,
-                            getResources().getDisplayMetrics()));
-                    */
-                    mLabelSize = ta.getDimensionPixelSize(attr, 14);
+                   /* mLabelSize = (int) ta.getDimension(attr, TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, 14,
+                            getResources().getDisplayMetrics()));*/
+                    mLabelSize = ta.getDimensionPixelSize(attr, dip2px(getContext(), 14));
+
                     break;
                 case R.styleable.bindding_item_content:
                     mContent = ta.getString(attr);
@@ -107,14 +98,13 @@ public class BiddingItemLayout extends FrameLayout {
                     mTvContentColor = ta.getColor(attr, getResources().getColor(R.color.blue_font));
                     break;
                 case R.styleable.bindding_item_content_tv_text_size:
-                    mTvContentSize = ta.getDimensionPixelSize(attr, 14);
+                    mTvContentSize = ta.getDimensionPixelSize(attr, dip2px(getContext(), 14));
                     break;
-
                 case R.styleable.bindding_item_content_ev_text_color:
                     mEtContentColor = ta.getColor(attr, getResources().getColor(R.color.blue_font));
                     break;
                 case R.styleable.bindding_item_content_ev_text_size:
-                    mEtContentSize = ta.getDimensionPixelSize(attr, 14);
+                    mEtContentSize = ta.getDimensionPixelSize(attr, dip2px(getContext(), 14));
                     break;
 
                 case R.styleable.bindding_item_content_hint:
@@ -124,22 +114,22 @@ public class BiddingItemLayout extends FrameLayout {
                     mHintColor = ta.getColor(attr, getResources().getColor(R.color.blue_font));
                     break;
                 case R.styleable.bindding_item_content_hint_size:
-                    mHintSize = ta.getDimensionPixelSize(attr, 14);
+                    mHintSize = ta.getDimensionPixelSize(attr, dip2px(getContext(), 14));
                     break;
-
                 case R.styleable.bindding_item_type:
                     mType = ta.getInt(attr, TYPE_TEXT);
                     break;
                 case R.styleable.bindding_item_img_right:
                     mImgRight = ta.getResourceId(attr, R.mipmap.ic_launcher);
                     break;
+                case R.styleable.bindding_item_img_visible:
+                    mImageRightVisible = ta.getBoolean(attr, false);
+                    break;
                 default:
                     break;
             }
         }
         ta.recycle();
-
-
         initView();
     }
 
@@ -155,17 +145,22 @@ public class BiddingItemLayout extends FrameLayout {
         if (!TextUtils.isEmpty(mLabelText)) {
             tvLabel.setText(mLabelText);
         }
-        tvLabel.setTextSize(mLabelSize);
+        tvLabel.setTextSize(TypedValue.COMPLEX_UNIT_PX, mLabelSize);
         tvLabel.setTextColor(mLabelColor);
 
 
+        float size = tvLabel.getTextSize();
+
+        Log.e("item", "mLabelSize:" + mLabelSize);
+        Log.e("item", "size:" + size);
         if (mType == TYPE_TEXT) {
             if (!TextUtils.isEmpty(mContent)) {
                 tvContent.setText(mContent);
             }
             tvContent.setTextColor(mTvContentColor);
-            tvContent.setTextSize(mTvContentSize);
-
+            tvContent.setTextSize(TypedValue.COMPLEX_UNIT_PX, mTvContentSize);
+            tvContent.setVisibility(VISIBLE);
+            etContent.setVisibility(GONE);
         } else {
             if (!TextUtils.isEmpty(mContent)) {
                 etContent.setText(mContent);
@@ -176,14 +171,111 @@ public class BiddingItemLayout extends FrameLayout {
                 etContent.setHintTextColor(mHintColor);
             }
             etContent.setTextColor(mEtContentColor);
-            etContent.setTextSize(mEtContentSize);
+            etContent.setTextSize(TypedValue.COMPLEX_UNIT_PX, mEtContentSize);
+            tvContent.setVisibility(GONE);
+            etContent.setVisibility(VISIBLE);
         }
 
-        if (mImgRight > 0) {
-            imgRight.setImageResource(mImgRight);
+        tvContent.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mIContentClickListener != null) {
+                    mIContentClickListener.click(v);
+                }
+            }
+        });
+
+        if (mImageRightVisible) {
+            imgRight.setOnClickListener(new OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (mIRightImgClickListener != null) {
+                        mIRightImgClickListener.click(v);
+                    }
+                }
+            });
+
+            if (mImgRight > 0) {
+                imgRight.setImageResource(mImgRight);
+            }
+
+            imgRight.setVisibility(VISIBLE);
+        } else {
+            imgRight.setVisibility(GONE);
         }
 
+    }
 
+
+    @Override
+    public RelativeLayout getRootView() {
+        return rootView;
+    }
+
+    public void setRootView(RelativeLayout rootView) {
+        this.rootView = rootView;
+    }
+
+    public TextView getTvLabel() {
+        return tvLabel;
+    }
+
+    public void setTvLabel(TextView tvLabel) {
+        this.tvLabel = tvLabel;
+    }
+
+    public TextView getTvContent() {
+        return tvContent;
+    }
+
+    public void setTvContent(TextView tvContent) {
+        this.tvContent = tvContent;
+    }
+
+    public EditText getEtContent() {
+        return etContent;
+    }
+
+    public void setEtContent(EditText etContent) {
+        this.etContent = etContent;
+    }
+
+    public ImageView getImgRight() {
+        return imgRight;
+    }
+
+    public void setImgRight(ImageView imgRight) {
+        this.imgRight = imgRight;
+    }
+
+    public View getLineView() {
+        return lineView;
+    }
+
+    public void setLineView(View lineView) {
+        this.lineView = lineView;
+    }
+
+    public int dip2px(Context context, float dpValue) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (dpValue * scale + 0.5f);
+    }
+
+
+    public void setIContentClickListener(IContentClickListener mIContentClickListener) {
+        this.mIContentClickListener = mIContentClickListener;
+    }
+
+    public void setIRightImgClickListener(IRightImgClickListener mIRightImgClickListener) {
+        this.mIRightImgClickListener = mIRightImgClickListener;
+    }
+
+    public interface IContentClickListener {
+        void click(View view);
+    }
+
+    public interface IRightImgClickListener {
+        void click(View view);
     }
 
 

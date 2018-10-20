9f36b0ad220109e5c192b31f7ffc7efd3628fb3b
guozhk <1565619659@qq.com>
2018-10-20 09:52:19 +0800

    test

---
+++ app/src/main/java/com/example/simple/model/smartrefreesh/SmartActivityActivity.java
@@ -0,0 +1,20 @@
+package com.example.simple.model.smartrefreesh;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.example.simple.ui.BaseActivity;
+
+public class SmartActivityActivity extends BaseActivity {
+
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.activity_smart_activity;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+}
